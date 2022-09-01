10ac3fac396eff76ee4ebf7a6755e68fd7f32cbc
Jan Warncke <janlothar@gmail.com>
2022-09-01 12:19:14 +0200

    conflict commit 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         print("Hello World!");
         print("Here is a conflict from a feature branch");
+        print("Here is another conflict from a feature branch");
     }
 
     public static void print(Object toPrint) {
