bf2eea17010db0b9f4bf5fd5cbee089bfcff7127
Jan Warncke <janlothar@gmail.com>
2022-09-01 12:17:35 +0200

    Hello World!

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        print("Hello World!");
+    }
+
+    public static void print(Object toPrint) {
+        System.out.println(toPrint);
+    }
+}
